d2b33ac6fbfc8202042c56f94dbcfd5bb7e67e9c
iboboshko <iboboshko@fix-price.ru>
2021-10-10 11:30:16 +0300

    Calculator

--- 01-basics/02-VueCalculator/script.js
+++ 01-basics/02-VueCalculator/script.js
@@ -1,3 +1,45 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp, defineComponent } from './vendor/vue.esm-browser.js';
 
-// Создайте Vue приложение
+const RootComponent = defineComponent({
+    name: 'Calculator',
+
+    data() {
+        return {
+            OPERATION_SUM: 'sum',
+            OPERATION_SUBTRACT: 'subtract',
+            OPERATION_MULTIPLY: 'multiply',
+            OPERATION_DIVIDE: 'divide',
+            operation: null,
+            operand1: 0,
+            operand2: 0,
+        };
+    },
+
+    computed: {
+        result() {
+            let result = 0;
+
+            switch (this.operation) {
+                case this.OPERATION_SUM:
+                    result = this.operand1 + this.operand2;
+                    break;
+                case this.OPERATION_SUBTRACT:
+                    result = this.operand1 - this.operand2;
+                    break;
+                case this.OPERATION_MULTIPLY:
+                    result = this.operand1 * this.operand2;
+                    break;
+                case this.OPERATION_DIVIDE:
+                    result = this.operand1 / this.operand2;
+                    break;
+                default:
+                    result = 0;
+            }
+
+            return result;
+        },
+    },
+});
+
+const app = createApp(RootComponent),
+    vm = app.mount('#app');
