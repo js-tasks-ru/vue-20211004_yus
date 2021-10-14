deac2d54a1c5eaf98c97d758102c25ca8b843d6e
iboboshko <iboboshko@fix-price.ru>
2021-10-14 17:15:33 +0300

    Разработка компонентов вывода митапов MeetupAgenda и MeetupAgendaItem

--- 02-components/05-MeetupAgenda/MeetupAgendaItem.js
+++ 02-components/05-MeetupAgenda/MeetupAgendaItem.js
@@ -1,23 +1,55 @@
 import { defineComponent } from './vendor/vue.esm-browser.js';
-// import { agendaItemIcons, agendaItemDefaultTitles } from './meetupService.js';
+import { agendaItemIcons, agendaItemDefaultTitles } from './meetupService.js';
+
+const TALK_TYPE = 'talk';
 
 export default defineComponent({
-  name: 'MeetupAgendaItem',
+    name: 'MeetupAgendaItem',
 
-  template: `
-    <div class="agenda-item">
-      <div class="agenda-item__col">
-        <img src="/assets/icons/icon-key.svg" class="icon" alt="key" />
-      </div>
-      <div class="agenda-item__col">00:00 - 00:00</div>
-      <div class="agenda-item__col">
-        <h3 class="agenda-item__title">Title</h3>
-        <p class="agenda-item__talk">
-          <span>Talk Speaker</span>
-          <span class="agenda-item__dot"></span>
-          <span class="agenda-item__lang">EN</span>
-        </p>
-        <p>Description</p>
-      </div>
-    </div>`,
+    props: {
+        agendaItem: {
+            type: Object,
+            required: true,
+        },
+    },
+
+    computed: {
+        title() {
+            return this.agendaItem.title || agendaItemDefaultTitles[this.type];
+        },
+
+        needTalkInfo() {
+            return this.type === TALK_TYPE;
+        },
+
+        icon() {
+            const iconName = agendaItemIcons[this.type];
+            return `/assets/icons/icon-${iconName}.svg`;
+        },
+
+        time() {
+            return `${this.agendaItem.startsAt} - ${this.agendaItem.endsAt}`;
+        },
+
+        type() {
+            return this.agendaItem.type;
+        },
+    },
+
+    template: `
+        <div class="agenda-item">
+          <div class="agenda-item__col">
+            <img :src="icon" class="icon" alt="key" />
+          </div>
+          <div class="agenda-item__col">{{ time }}</div>
+          <div class="agenda-item__col">
+            <h3 class="agenda-item__title">{{ title }}</h3>
+            <p class="agenda-item__talk" v-if="needTalkInfo">
+              <span>{{ agendaItem.speaker }}</span>
+              <span class="agenda-item__dot"></span>
+              <span class="agenda-item__lang">{{ agendaItem.language }}</span>
+            </p>
+            <p v-if="agendaItem.description">{{ agendaItem.description }}</p>
+          </div>
+        </div>`,
 });
