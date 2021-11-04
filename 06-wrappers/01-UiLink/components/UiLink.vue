3a5173b2f1c85bee3caed5b36d350ab4647c5b95
iboboshko <iboboshko@fix-price.ru>
2021-11-04 10:56:25 +0300

    Универсальный компонент вывода текстовых ссылок UiLink

--- 06-wrappers/01-UiLink/components/UiLink.vue
+++ 06-wrappers/01-UiLink/components/UiLink.vue
@@ -1,20 +1,30 @@
 <template>
-  <a class="link">Link!</a>
+    <component :is="tag" class="link">
+        <slot></slot>
+    </component>
 </template>
 
 <script>
+import { RouterLink } from 'vue-router';
+
 export default {
-  name: 'UiLink',
+    name: 'UiLink',
+
+    props: {
+        tag: {
+            default: RouterLink,
+        },
+    },
 };
 </script>
 
 <style scoped>
 .link {
-  color: var(--blue);
-  text-decoration: none;
+    color: var(--blue);
+    text-decoration: none;
 }
 
 .link:hover {
-  text-decoration: underline;
+    text-decoration: underline;
 }
 </style>
