81d757c2b02d62eb2e586bb120d0a6c594351d52
iboboshko <iboboshko@fix-price.ru>
2021-11-11 11:51:56 +0300

    Реализация компонента ввода UiInput

--- 06-wrappers/03-UiInput/components/UiInput.vue
+++ 06-wrappers/03-UiInput/components/UiInput.vue
@@ -1,95 +1,177 @@
 <template>
-  <div class="input-group input-group_icon input-group_icon-left input-group_icon-right">
-    <div class="input-group__icon">
-      <img class="icon" alt="icon" />
-    </div>
+    <div
+        class="input-group"
+        :class="mainBlockClass"
+    >
+        <div
+            v-if="$slots['left-icon']"
+            class="input-group__icon"
+        >
+            <slot name="left-icon"></slot>
+        </div>
 
-    <input ref="input" class="form-control form-control_rounded form-control_sm" />
+        <textarea
+            v-if="multiline"
+            v-bind="$attrs"
+            :value="modelValue"
+            @[fieldEvent]="$emit('update:modelValue', $event.target.value)"
+            ref="input"
+            class="form-control"
+        ></textarea>
 
-    <div class="input-group__icon">
-      <img class="icon" alt="icon" />
+        <input
+            v-else
+            :class="inputClass"
+            v-bind="$attrs"
+            :value="modelValue"
+            @[fieldEvent]="$emit('update:modelValue', $event.target.value)"
+            ref="input"
+            class="form-control"
+        />
+
+        <div
+            v-if="$slots['right-icon']"
+            class="input-group__icon"
+        >
+            <slot name="right-icon"></slot>
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
 export default {
-  name: 'UiInput',
+    name: 'UiInput',
+
+    inheritAttrs: false,
+
+    emits: ['update:modelValue'],
+
+    props: {
+        modelValue: {
+            default: '',
+        },
+
+        modelModifiers: {
+            default: () => ({}),
+        },
+
+        small: {
+            type: Boolean,
+        },
+
+        rounded: {
+            type: Boolean,
+        },
+
+        multiline: {
+            type: Boolean,
+        },
+    },
+
+    computed: {
+        fieldEvent() {
+            return this.modelModifiers.lazy ? 'change' : 'input';
+        },
+
+        inputClass() {
+            return {
+                'form-control_sm': this.small,
+                'form-control_rounded': this.rounded,
+            };
+        },
+
+        mainBlockClass() {
+            const existLeftIcon = !!this.$slots['left-icon'],
+                existRightIcon = !!this.$slots['right-icon'];
+
+            return {
+                'input-group_icon': existLeftIcon || existRightIcon,
+                'input-group_icon-left': existLeftIcon,
+                'input-group_icon-right': existRightIcon,
+            };
+        },
+    },
+
+    methods: {
+        focus() {
+            this.$refs.input.focus();
+        },
+    },
 };
 </script>
 
 <style scoped>
 .form-control {
-  padding: 12px 16px;
-  height: 52px;
-  border-radius: 8px;
-  border: 2px solid var(--blue-light);
-  font-family: 'Nunito', sans-serif;
-  font-weight: 600;
-  font-size: 20px;
-  line-height: 28px;
-  color: var(--body-color);
-  transition: 0.2s border-color;
-  background-color: var(--white);
-  outline: none;
-  box-shadow: none;
+    padding: 12px 16px;
+    height: 52px;
+    border-radius: 8px;
+    border: 2px solid var(--blue-light);
+    font-family: 'Nunito', sans-serif;
+    font-weight: 600;
+    font-size: 20px;
+    line-height: 28px;
+    color: var(--body-color);
+    transition: 0.2s border-color;
+    background-color: var(--white);
+    outline: none;
+    box-shadow: none;
 }
 
 .form-control::placeholder {
-  font-weight: 400;
-  color: var(--blue-2);
+    font-weight: 400;
+    color: var(--blue-2);
 }
 
 .form-control:focus {
-  border-color: var(--blue);
+    border-color: var(--blue);
 }
 
 textarea.form-control {
-  width: 100%;
-  min-height: 211px;
+    width: 100%;
+    min-height: 211px;
 }
 
 .form-control.form-control_rounded {
-  border-radius: 26px;
+    border-radius: 26px;
 }
 
 .form-control.form-control_sm.form-control_rounded {
-  border-radius: 22px;
+    border-radius: 22px;
 }
 
 .form-control.form-control_sm {
-  padding: 8px 16px;
-  height: 44px;
-  border-radius: 4px;
+    padding: 8px 16px;
+    height: 44px;
+    border-radius: 4px;
 }
 
 .input-group {
-  position: relative;
+    position: relative;
 }
 
 .input-group .form-control {
-  width: 100%;
+    width: 100%;
 }
 
 .input-group.input-group_icon-left .form-control {
-  padding-left: 50px;
+    padding-left: 50px;
 }
 
 .input-group.input-group_icon-right .form-control {
-  padding-right: 50px;
+    padding-right: 50px;
 }
 
 .input-group.input-group_icon .input-group__icon {
-  position: absolute;
-  top: 50%;
-  transform: translate(0, -50%);
+    position: absolute;
+    top: 50%;
+    transform: translate(0, -50%);
 }
 
 .input-group.input-group_icon.input-group_icon-left .input-group__icon:first-child {
-  left: 16px;
+    left: 16px;
 }
 
 .input-group.input-group_icon.input-group_icon-right .input-group__icon:last-child {
-  right: 16px;
+    right: 16px;
 }
 </style>
