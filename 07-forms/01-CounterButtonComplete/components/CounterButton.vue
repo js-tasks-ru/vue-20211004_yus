1a742d8d078f2908c06789c471f31c9195e69cfe
iboboshko <iboboshko@fix-price.ru>
2022-01-08 12:08:34 +0300

    Самостоятельный CounterButton

--- 07-forms/01-CounterButtonComplete/components/CounterButton.vue
+++ 07-forms/01-CounterButtonComplete/components/CounterButton.vue
@@ -1,9 +1,32 @@
 <template>
-  <button></button>
+  <button
+    @click="$emit('update:count', ++innerCount)"
+  >{{ innerCount }}</button>
 </template>
 
 <script>
 export default {
   name: 'CounterButton',
+
+  props: {
+    count: {
+      type: Number,
+      default: 0,
+    },
+  },
+
+  emits: ['update:count'],
+
+  data() {
+    return {
+      innerCount: this.count,
+    };
+  },
+
+  watch: {
+    count(newValue) {
+      this.innerCount = newValue;
+    },
+  },
 };
 </script>
